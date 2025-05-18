085d141ceb489a14d1c25ec086c9fa13fc787c17
Mikhail Nosov <manosov1984@gmail.com>
2025-05-18 12:41:03 +0300

    feat: add messages types

---
+++ src/types/types-messages.ts
@@ -0,0 +1,45 @@
+import { MESSAGE_TYPES } from '../messages-types';
+
+import type { Position, Ship } from './types-game';
+
+type Body<T, V extends string> = {
+  type: V;
+  data: T;
+  id: number;
+};
+
+type LoginOrCreatePlayerDataRequest = {
+  name: string;
+  password: string;
+};
+
+export type LoginOrCreatePlayerRequest = Body<
+  LoginOrCreatePlayerDataRequest,
+  typeof MESSAGE_TYPES.REG
+>;
+
+type AddUserToRoomData = {
+  indexRoom: number | string;
+};
+
+export type AddUserToRoom = Body<
+  AddUserToRoomData,
+  typeof MESSAGE_TYPES.ADD_USER_TO_ROOM
+>;
+
+type ShipsData = {
+  gameId: number | string;
+  indexPlayer: number | string;
+  ships: Ship[];
+};
+
+export type AddShips = Body<ShipsData, typeof MESSAGE_TYPES.ADD_SHIPS>;
+
+export type AttackData = Position & {
+  gameId: number | string;
+  indexPlayer: number | string;
+};
+
+export type Attack = Body<AttackData, typeof MESSAGE_TYPES.ATTACK>;
+
+export type AttackFeedbackStatus = 'miss' | 'killed' | 'shot';
